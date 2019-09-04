2ef7c9ffeab692dffc230f1595c42e7b416b524e
gmbh77@mail.ru <gmbh77@mail.ru>
2019-09-04 20:07:51 +0300

    Ok

---
+++ URLChecker/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace URLChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(589, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start_check_1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 211);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(538, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "36a8Zax7na";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(538, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Опорное значение хэша  (для url - https://anonfile.com/36a8Zax7na/filecost_txt   " +
+    "-   хэш будет - 36a8Zax7na)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "URLChecker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ URLChecker/HttpBruteForce.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLChecker
+{
+    public class HttpBruteForce2
+    {
+        private readonly int _parralelCount;
+        private readonly Stack<string> _urls;
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        private static HttpClientHandler handler = new HttpClientHandler() { MaxConnectionsPerServer = 65000 };
+        private static HttpClient _client;
+
+
+        public HttpBruteForce2(Stack<string> urls, string pathResult, int parralelCount = 10)
+        {
+            _parralelCount = parralelCount;
+            _urls = urls;
+            _client = new HttpClient(handler);
+            ServicePointManager.DefaultConnectionLimit = 65000;
+        }
+
+
+        public void StartBruteForce()
+        {
+            List<Task> tasks = new List<Task>();
+
+            while (_urls.Count > 0)
+            {
+                tasks.Add(brutForceAsync(_urls.Pop()));
+
+                if (tasks.Count > _parralelCount)
+                {
+                    Task.WaitAny /*WhenAny*/(tasks.ToArray());                  //с WaitAny - WaitAll    отрабатывает один проход и останавливается, около 35 запросов в секунду в секунду
+                    tasks = CleanFinishTasks(tasks);                            //с WhenAny - WhenAll    возвращаемся из объекта сразу и лог не создается
+                }                                                               //с WhenAny - WhenAll    добавили в Task.Run --   .ConfigureAwait(false)  тот же результат   лог не создается
+            }                                                                   //с WaitAny - WaitAll    добавили в Task.Run --   .ConfigureAwait(false)  отрабатывает как нужно по циклу
+                                                                                //                         около 50 - 70 запросов в секунду насчитал
+                                                                                // но когда лог достигает около 700 Кб происходит ошибка на 49 строке
+
+            if (tasks.Count > 0)
+            {
+                tasks = CleanFinishTasks(tasks);
+                Task.WaitAll /*WhenAll*/(tasks.ToArray());
+            }
+
+        }
+
+        private List<Task> CleanFinishTasks(List<Task> tasks)
+        {
+            List<Task> cleanTaskList = new List<Task>();
+
+            foreach (var task in tasks)
+            {
+                if (!task.IsCompleted && !task.IsFaulted)
+                {
+                    cleanTaskList.Add(task);
+                }
+            }
+
+            return cleanTaskList;
+        }
+
+        private async Task brutForceAsync(string webUrl)
+        {
+            await Task.Run(async () =>
+             {
+                 try
+                 {
+                     HttpResponseMessage response = await _client.GetAsync(webUrl);
+                     response.EnsureSuccessStatusCode();
+                     string responseBody = await response.Content.ReadAsStringAsync();
+
+                     logger.Info($"ok|{webUrl}");
+                 }
+                 catch (HttpRequestException e)
+                 {
+                     logger.Error(e, $"{e.Message}|{webUrl}");
+                 }
+             }).ConfigureAwait(false);
+        }
+    }
+}
